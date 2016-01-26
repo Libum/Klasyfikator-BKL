e2954e308b22ca6abad230175787d2d3510a1382
Libum <liburam@gmail.com>
2016-01-26 17:47:18 +0100

    W Tag_Analysis: - stworzenie funkcji simplify_tags, upraszczającej tagowanie gramatyczne

--- Tag_Analysis.R
+++ Tag_Analysis.R
@@ -1,4 +1,23 @@
+#Funckcja upraszczająca tagi (sprowadza je do podstawowych klas: rzeczownik, czasownik, przymiotnik itp.)
+
+simplify_tags = function(sentence){
+        sentence = gsub(x = sentence, pattern = "depr", replacement = "subst")
+        sentence = gsub(x = sentence, pattern = "(adja)|(adjp)", replacement = "adj")
+        sentence = gsub(x = sentence,
+                        pattern = "(fin)|(nbedzie)|(naglt)|(npraet)|(nimpt)|(nimps)|(ninf)|
+                        (npcon)|(npant)|(nger)|(npact)|(nppas)|(winien)",
+                        replacement = "verb")
+        sentence = gsub(x = sentence, pattern = "(adja)|(adjp)", replacement = "adj")
+        sentence = gsub(x = sentence, pattern = "(ppron12)|(ppron3)|(siebie)", replacement = "ppron")
+}
+
+sko2014$Tagi = sapply(sko2014$Tagi, FUN = simplify_tags)
+sko2015$Tagi = sapply(sko2015$Tagi, FUN = simplify_tags)
+uza2014$Tagi = sapply(uza2014$Tagi, FUN = simplify_tags)
+uza2015$Tagi = sapply(uza2015$Tagi, FUN = simplify_tags)
+
 #Stworzenie n-gramów przy użyciu tagów
+
 library(RWeka)
 library(tm)
 
